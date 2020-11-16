cf7128ee6d1d6e0a98405169c29e492a4a8bb890
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-16 11:47:02 +0100

    Added readme file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
+from db import Base, engine, session
+from models.customers import Customer
+from models.orders import Order
+from models.parents import Parent
+from models.children import Child
 
 
 def main():
-    pass
+    Base.metadata.create_all(engine)
 
 
 if __name__ == '__main__':
     main()
-x
