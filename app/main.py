85cad442c17b755486b40ca716e6e128fcc3803b
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-17 11:15:08 +0100

    Changes in main show_test method

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 from db import Base, engine, session
-from models.parents import Parent
-from models.children import Child
 from models.address import Address
 from models.customer import Customer
 
@@ -15,8 +13,10 @@ def add_customer():
     country = input('Enter country: ')
 
     address = Address(address=address, city=city, postalCode=postal_code, country=country)
-    customer = Customer(first_name=first_name, last_name=last_name, customer_address_id=1)
     session.add(address)
+    session.commit()
+
+    customer = Customer(first_name=first_name, last_name=last_name, customer_address_id=address.address_id)
     session.add(customer)
     session.commit()
 
@@ -28,17 +28,41 @@ def show_all_customers():
 
 
 def show_all_addresses():
-    addresses = session.query(Address).all()
-    for address in addresses:
-        print(address)
+    pass
+
+
+def show_test():
+    records = session.query(Customer).\
+        join(Address, Customer.customer_address_id == Address.address_id).all()
+    for record in records:
+        record_object = {
+            'Name': record.first_name,
+            'Address': record.address.address,
+            'City': record.address.city
+        }
+        print(record_object)
+
+    for cname, customer_address_id, address_id in session.query(Customer.first_name, Customer.customer_address_id,
+                                                                Address.address_id):
+        print(cname, customer_address_id, address_id)
+
+    for row in session.query(Customer, Customer.first_name).all():
+        print(row.Customer, row.first_name)
+
+    for c, a in session.query(Customer, Address). \
+            filter(Customer.customer_address_id). \
+            filter(Address.address_id). \
+            all():
+        print(c, a)
 
 
 def main():
     Base.metadata.create_all(engine)
 
-    add_customer()
-    show_all_customers()
-
+    # add_customer()
+    # show_all_customers()
+    # show_all_addresses()
+    show_test()
 
 if __name__ == '__main__':
     main()
