48d001284a4204712a23761065f2781778d22c90
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-17 20:38:24 +0100

    Created car_brands table

--- app/models/car_brand.py
+++ app/models/car_brand.py
@@ -1,6 +1,15 @@
-def main():
-    pass
+from db import Base
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship
 
 
-if __name__ == '__main__':
-    main()
+class CarBrand(Base):
+    __tablename__ = 'car_brands'
+
+    car_brand_id = sa.Column(sa.Integer, primary_key=True)
+    car_brand_name = sa.Column(sa.String(50), nullable=False, unique=True)
+
+    car_models = relationship("CarModel", back_populates="car_brands")
+
+    def __repr__(self):
+        return f'Car model: {self.car_brand_id}, {self.car_brand_name}'
