dad4d81946a7c59c0db1a3ecb92fae1ce271177c
Emelie Almqvist <emeline.andersson@live.se>
2020-11-17 13:47:19 +0100

    Added not nullable in Order model

--- app/models/order.py
+++ app/models/order.py
@@ -4,11 +4,11 @@ import sqlalchemy as sa
 class Order(Base):
     __tablename__ = 'orders'
 
-    order_id = sa.Column(sa.Integer, primary_key=True)
-    order_date = sa.Column(sa.Date)
-    required_date = sa.Column(sa.Date)
-    shipped_date = sa.Column(sa.Date)
-    status = sa.Column(sa.String(50))
+    order_id = sa.Column(sa.Integer, nullable=False, primary_key=True)
+    order_date = sa.Column(sa.Date, nullable=False)
+    required_date = sa.Column(sa.Date, nullable=False)
+    shipped_date = sa.Column(sa.Date, nullable=False)
+    status = sa.Column(sa.String(50), nullable=False)
     comment = sa.Column(sa.String(50))
-    employee_id = sa.Column(sa.Integer, sa.ForeignKey('employees.employee_id'))
-    customer_id = sa.Column(sa.Integer, sa.ForeignKey('customers.customer_id'))
+    employee_id = sa.Column(sa.Integer, sa.ForeignKey('employees.employee_id'), nullable=False)
+    customer_id = sa.Column(sa.Integer, sa.ForeignKey('customers.customer_id'), nullable=False)
