9a49bc0bedcd239659a0aa011fcaa1bc4c412bd1
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-20 14:01:24 +0100

    Bug fix of employee_ide -> employee_id

--- app/Data/models/employee.py
+++ app/Data/models/employee.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import relationship
 class Employee(Base):
     __tablename__ = 'employees'
 
-    employee_ide = sa.Column(sa.Integer, primary_key=True)
+    employee_id = sa.Column(sa.Integer, primary_key=True)
     employee_name = sa.Column(sa.String(50), nullable=False)
     employee_store_id = sa.Column(sa.Integer, sa.ForeignKey('stores.store_id'), nullable=False)
 
