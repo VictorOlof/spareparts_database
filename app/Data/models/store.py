bd881fcbb5e892c209d3de8366b50c5f15ce5ae0
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-20 13:23:51 +0100

    Bug fix of primary key

--- app/Data/models/store.py
+++ app/Data/models/store.py
@@ -2,10 +2,11 @@ from Data.db import Base
 import sqlalchemy as sa
 from sqlalchemy.orm import relationship
 
+
 class Store(Base):
     __tablename__ = 'stores'
 
-    store_id = sa.Column(sa.Integer, pirmary_key=True)
+    store_id = sa.Column(sa.Integer, primary_key=True)
     store_name = sa.Column(sa.String(50), nullable=False)
 
     def __repr__(self):
