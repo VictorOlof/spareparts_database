edbdb1625d8dcb9aa5fe12d0f174ce08c5d209f9
Emelie Almqvist <emeline.andersson@live.se>
2020-11-23 11:15:49 +0100

    Added Categories Menu

---
+++ app/Controllers/categories_controller.py
@@ -0,0 +1,14 @@
+import Data.Repository.category_repository as ca
+
+
+def add_category(category_id, category_name, category_description):
+    ca.add_category(category_id, category_name, category_description)
+
+
+def get_all_categories():
+    return ca.get_all_categories()
+
+
+def get_products_by_category():
+    return ca.get_products_by_category()
+    pass
