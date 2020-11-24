bda7ea0af4885db15b4254e16604d0276865ec22
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-24 11:56:35 +0100

    Added menu option to view all car models

--- app/UI/car_menu.py
+++ app/UI/car_menu.py
@@ -1,7 +1,7 @@
 from Controllers.customer_car_controller import add_customer_car, get_customer_car_by_id
 from Controllers.product_car_controller import add_product_car
-from Controllers.carbrands_controller import add_car_brand
-from Controllers.car_model_controller import add_car_model
+from Controllers.carbrands_controller import add_car_brand, get_all_car_brands
+from Controllers.car_model_controller import add_car_model, get_all_car_models
 
 
 def car_menu():
@@ -14,10 +14,11 @@ def car_menu():
         print("4. Create new car model")
         print("5. View cars for customer")
         print("6. View car models for product")
+        print("7. View all car models")
 
         select = input("> ")
 
-        if select == "1":
+        if select == "1":  # Add car to customer
             customer_id = int(input('Enter customer id: '))
             car_model_id = int(input('Enter car model id: '))
             reg_plate = input("Enter registration plate: ")
@@ -25,31 +26,38 @@ def car_menu():
 
             add_customer_car(customer_id, car_model_id, reg_plate, color)
 
-        elif select == "2":
+        elif select == "2":  # Add car to product
             product_id = int(input('Enter product id: '))
             car_model_id = int(input('Enter car model id: '))
 
             add_product_car(product_id, car_model_id)
 
-        elif select == "3":
+        elif select == "3":  # Create new car brand
             car_brand_name = input("Enter brand name: ")
             add_car_brand(car_brand_name=car_brand_name)
 
-        elif select == "4":
+        elif select == "4":  # Create new car model
             car_model_name = input("Enter model name: ")
             car_model_year = input("Enter model year: ")
             car_model_car_brand_id = int(input('Enter brand id: '))
 
             add_car_model(car_model_name, car_model_year, car_model_car_brand_id)
 
-        elif select == "5":
-            customer_id = input("Enter customer id: ")
-            customer_cars = get_customer_car_by_id(customer_id)
-            for customer_car in customer_cars:
-                print(customer_car)
-
-        elif select == "6":
+        elif select == "5":  # View cars for customer
             pass
+
+        elif select == "6":  # View car models for product
+            pass
+
+        elif select == "7":  # View all car models
+            car_brands = get_all_car_brands()
+
+            for cb in car_brands:
+                print(cb.car_brand_name)
+                for model in cb.car_models:
+                    print(f'  {model.car_model_name} | {model.car_model_year}')
 
         else:
             break
+
+car_menu()
