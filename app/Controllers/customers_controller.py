5229bb5b76ad968d3be135ad3d3fe1ef9965e291
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-24 19:29:28 +0100

    Added ability to update address for customer

--- app/Controllers/customers_controller.py
+++ app/Controllers/customers_controller.py
@@ -7,3 +7,11 @@ def add_customer(first_name, last_name, address_name, city, postal_code, country
 
 def get_all_customers():
     return cr.get_all_customers()
+
+
+def update_customer_address(obj, address, city, postal_code, country):
+    cr.update_customer_address(obj, address, city, postal_code, country)
+
+
+def get_customer_by_id(customer_id):
+    return cr.get_customer_by_id(customer_id)
--- app/UI/customers_menu.py
+++ app/UI/customers_menu.py
@@ -1,4 +1,5 @@
-from Controllers.customers_controller import add_customer, get_all_customers
+from Controllers.customers_controller import add_customer, get_all_customers, \
+    get_customer_by_id, update_customer_address
 
 
 def customers_menu():
@@ -7,21 +8,33 @@ def customers_menu():
         print("----------")
         print("1. Add info to customers")
         print("2. Show all customers")
+        print("3. Update address for customer")
 
         select = input("> ")
         if select == "1":
             first_name = input("Enter first name: ")
             last_name = input("Enter last name: ")
-            address_name = input('Enter street address: ')
+            address = input('Enter street address: ')
             city = input('Enter city: ')
             postal_code = int(input('Enter postal code: '))
             country = input('Enter country: ')
 
-            add_customer(first_name, last_name, address_name, city, postal_code, country)
+            add_customer(first_name, last_name, address, city, postal_code, country)
 
         elif select == "2":
             customers = get_all_customers()
             for customer in customers:
                 print(customer)
+
+        elif select == "3":  # Update address for customer
+            customer_id = input("Enter customer id: ")
+            customer = get_customer_by_id(customer_id)
+            address = input('Enter street address: ')
+            city = input('Enter city: ')
+            postal_code = input('Enter postal code: ')
+            country = input('Enter country: ')
+            update_customer_address(customer, address, city, postal_code, country)
+
         else:
             break
+
