44d8237236d50d580e12731b107d0b1487b53546
Emelie Almqvist <emeline.andersson@live.se>
2020-11-25 14:28:52 +0100

    Updated Orders Menu

--- app/UI/main_menu.py
+++ app/UI/main_menu.py
@@ -1,3 +1,4 @@
+from UI.categories_menu import categories_menu
 from UI.customers_menu import customers_menu
 from UI.orders_menu import orders_menu
 from UI.car_menu import car_menu
@@ -8,8 +9,9 @@ def main_menu():
         print("Main menu")
         print("----------")
         print("1. Customers")
-        print("2. Orders")
+        print("2. Orders menu")
         print("3. Cars menu")
+        print("4. Categories menu")
 
         select = input("> ")
         if select == "1":
@@ -18,5 +20,7 @@ def main_menu():
             orders_menu()
         elif select == "3":
             car_menu()
+        elif select == "4":
+            categories_menu()
         else:
             break
--- app/UI/orders_menu.py
+++ app/UI/orders_menu.py
@@ -1,14 +1,18 @@
-from Controllers.orders_controller import add_order, get_all_orders
+from Controllers.order_details_controller import get_all_order_details
+from Controllers.orders_controller import create_order, get_all_orders, create_employee, create_store, add_item_to_order
 
 
 def orders_menu():
     while True:
         print("Orders Menu")
         print("===========")
-        print("1. Add Info To Orders")
-        print("2. View All Orders")
-
-        print("5. Quit Orders Menu")
+        print("1. Create New Order")
+        print("2. Create New Employee")
+        print("3. Create New Store")
+        print("4. Add Item To Order")
+        print("5. View All Items For Order")
+        print("6. View All Orders")
+        print("7. Quit Orders Menu")
 
         selection = input(" >")
         if selection == "1":
@@ -20,11 +24,35 @@ def orders_menu():
             employee_id = int(input('Employee id: '))
             customer_id = int(input('Customer id: '))
 
-            add_order(order_date, required_date, shipped_date, status, comment, employee_id, customer_id)
+            create_order(order_date, required_date, shipped_date, status, comment, employee_id, customer_id)
 
         elif selection == "2":
+            employee_name = input('Employee name: ')
+            employee_store_id = int(input('Store id: '))
+
+            create_employee(employee_name, employee_store_id)
+
+        elif selection == "3":
+            store_name = input('Store name: ')
+
+            create_store(store_name)
+
+        elif selection == "4":
+            order_id = int(input('Order id: '))
+            product_id = int(input('Product id: '))
+            quantity_ordered = int(input('Quantity ordered: '))
+            sell_price_each = float(input('Sell price each: '))
+
+            add_item_to_order(order_id, product_id, quantity_ordered, sell_price_each)
+
+        elif selection == "5":
+            order_id = int(input("Order id: "))
+            order_details = get_all_order_details(order_id)
+            print(order_details)
+
+        elif selection == "6":
             orders = get_all_orders()
             for order in orders:
                 print(order)
-        else:
+        elif selection == "7":
             break
