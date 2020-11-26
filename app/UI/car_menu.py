9bba2436a35bd1920ae6995546a526fc6df7884a
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-26 18:16:51 +0100

    Updated menu for selection 5-7

--- app/UI/car_menu.py
+++ app/UI/car_menu.py
@@ -1,8 +1,9 @@
-from Controllers.customer_car_controller import add_customer_car, get_car_by_reg_plate
+from Controllers.customer_car_controller import add_customer_car, get_all_customer_car, get_car_by_reg_plate
 from Controllers.product_car_controller import add_product_car
-from Controllers.carbrands_controller import add_car_brand, get_all_car_brands
-from Controllers.car_model_controller import add_car_model, get_all_car_models_by_brand
-from Controllers.customers_controller import get_all_customers
+from Controllers.carbrands_controller import add_car_brand, get_all_car_brands, get_brand_by_model
+from Controllers.car_model_controller import add_car_model, get_all_car_models_by_brand, get_model_by_car, \
+    get_all_car_models
+from Controllers.customers_controller import get_customer_by_car
 
 
 def car_menu():
@@ -42,40 +43,44 @@ def car_menu():
             car_model_car_brand_id = int(input('Enter brand id: '))
             add_car_model(car_model_name, car_model_year, car_model_car_brand_id)
 
-        # TODO: edit select 5-7
         elif select == "5":  # Search car owner by reg. plate
             reg_plate = input("Enter reg. plate: ")
-            car = get_car_by_reg_plate(reg_plate)
-            if car:
+            customer_car = get_car_by_reg_plate(reg_plate)
+            if customer_car:
+                customer = get_customer_by_car(customer_car)
                 print('{:12}{:12}{:12}'.format('Reg. Plate', 'First name', 'Last name'))
-                print('{:12}{:12}{:12}'.format(car.reg_plate.upper(),
-                                               car.customer.first_name.capitalize(),
-                                               car.customer.last_name.capitalize()))
+                print('{:12}{:12}{:12}'.format(customer_car.reg_plate.upper(),
+                                               customer.first_name.capitalize(),
+                                               customer.last_name.capitalize()))
             else:
                 print(f"Could not found any owner with reg. plate: {reg_plate.upper()}")
 
         elif select == "6":  # View owners for all cars
-            customers = get_all_customers()
-            for cu in customers:
-                if cu.cars:
-                    print('{:12}{:12}{:12}{:12}{:12}{:12}'.format('Reg. Plate', 'Car brand', 'Car model', 'Car color',
-                                                                  'First name', 'Last name'))
-                    for cc in cu.cars:
-                        print('{:12}{:12}{:12}{:12}{:12}{:12}'.format(cc.reg_plate.upper(),
-                                                                      cc.car_model.car_brand.
-                                                                      car_brand_name.capitalize(),
-                                                                      cc.car_model.car_model_name,
-                                                                      cc.color.capitalize(),
-                                                                      cu.first_name, cu.last_name))
+            customer_cars = get_all_customer_car()
+            if customer_cars:
+                for customer_car in customer_cars:
+                    customer = get_customer_by_car(customer_car)
+                    model = get_model_by_car(customer_car)
+                    print('{:12}{:12}{:12}{:12}{:12}'.format('Reg. Plate', 'Model', 'Year', 'First name', 'Last name'))
+                    print('{:12}{:12}{:12}{:12}{:12}'.format(customer_car.reg_plate.upper(),
+                                                             model.car_model_name.capitalize(),
+                                                             str(model.car_model_year),
+                                                             customer.first_name.capitalize(),
+                                                             customer.last_name.capitalize()))
+            else:
+                print("Could not find any cars.")
 
         elif select == "7":  # View all car models
-            car_brands = get_all_car_brands()
-            print('{:12}{:12}{}'.format('Brand', 'Model', 'Year'))
-            for key, cb in car_brands.items():
-                for cm in cb.car_models:
-                    print('{:12}{:12}{}'.format(cb.car_brand_name.capitalize(),
-                                                cm.car_model_name,
-                                                cm.car_model_year))
+            car_models = get_all_car_models()
+            if car_models:
+                print('{:12}{:12}{}'.format('Brand', 'Model', 'Year'))
+                for car_model in car_models:
+                    car_brand = get_brand_by_model(car_model)
+                    print('{:12}{:12}{}'.format(car_brand.car_brand_name.capitalize(),
+                                                car_model.car_model_name,
+                                                car_model.car_model_year))
+            else:
+                print("Could not find any car models")
 
         elif select == "8":  # View all car models for car brand
             car_brands = get_all_car_brands()
