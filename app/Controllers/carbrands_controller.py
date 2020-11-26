72b8c350c09622d5ce0d9545b198461625f733dc
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-26 12:11:05 +0100

    Added function to view aöö car models for specific car brand

--- app/Controllers/carbrands_controller.py
+++ app/Controllers/carbrands_controller.py
@@ -6,4 +6,5 @@ def add_car_brand(car_brand_name):
 
 
 def get_all_car_brands():
-    return cbr.get_all_car_brands()
+    car_brands = cbr.get_all_car_brands()
+    return {i+1: car_brand for i, car_brand in enumerate(car_brands)}
