50f4cf647f78bb841140e51a4d98b04e44df2ca7
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-26 19:04:22 +0100

    Renamed get_user_menu_option -> get_user_option_by_dict_keys

--- app/UI/menu_functions.py
+++ app/UI/menu_functions.py
@@ -3,7 +3,7 @@ def print_menu(menu_options: dict):
         print(f"{key}. {menu_options[key][0]}")
 
 
-def get_user_menu_option(menu_options: dict):
+def get_user_option_by_dict_keys(menu_options: dict):
     while True:
         try:
             value = input("> ")
