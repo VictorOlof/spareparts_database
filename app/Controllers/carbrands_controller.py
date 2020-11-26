a887beaa926f469e08b5c5849052dbd5fa7bd136
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-26 18:14:39 +0100

    Added get_brand_by_model

--- app/Controllers/carbrands_controller.py
+++ app/Controllers/carbrands_controller.py
@@ -8,3 +8,7 @@ def add_car_brand(car_brand_name):
 def get_all_car_brands():
     car_brands = cbr.get_all_car_brands()
     return {i+1: car_brand for i, car_brand in enumerate(car_brands)}
+
+
+def get_brand_by_model(car_model_obj):
+    return cbr.get_brand_by_model(car_model_obj)
