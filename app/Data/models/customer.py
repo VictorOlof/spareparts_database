73d3da1023568a87e437a8401b35451ce750f9c4
Victor Olofsson <victorolofsson@hotmail.com>
2020-11-30 14:25:40 +0100

    Added ability to remove customer

--- app/Data/models/customer.py
+++ app/Data/models/customer.py
@@ -16,8 +16,8 @@ class Customer(Base):
     company_name = sa.Column(sa.String(100), nullable=True)
     org_number = sa.Column(sa.String(100), nullable=True)
 
-    cars = relationship("CustomerCar", back_populates="customer", cascade="all, delete, delete-orphan")
-    orders = relationship("Order", back_populates="customer")
+    cars = relationship("CustomerCar", back_populates="customer", cascade="all, delete")
+    orders = relationship("Order", back_populates="customer", cascade="all, delete")
 
     def __repr__(self):
         return f'Customer: {self.customer_id}, {self.first_name}, {self.last_name}, {self.address}, {self.city}, {self.postal_code}, ' \
