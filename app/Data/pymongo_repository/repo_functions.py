70faae15663fd2a5237971d07a17d384faae3324
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-04 13:44:08 +0100

    Added repo_functions for pymongo

---
+++ app/Data/pymongo_repository/repo_functions.py
@@ -0,0 +1,27 @@
+from bson import ObjectId
+
+
+def get_all_models(model_obj):
+    try:
+        return model_obj.all()
+    except:
+        return None
+
+
+def get_model_by_id(model_obj, value):
+    try:
+        return model_obj.find(_id=ObjectId(value)).first_or_none()
+    except:
+        return None
+
+
+def get_models_by_column_value(model_obj, column_value, value):
+    pass
+
+
+def update_object_by_column(model_obj, column_value, value):
+    pass
+
+
+def remove_object(model_obj):
+    pass
