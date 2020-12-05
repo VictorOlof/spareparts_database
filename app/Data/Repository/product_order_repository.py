2eb0b6964e5733edc412b40f66a7baf5f8177883
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-05 12:19:00 +0100

    Added get_product_order_by_id

--- app/Data/Repository/product_order_repository.py
+++ app/Data/Repository/product_order_repository.py
@@ -5,3 +5,7 @@ from Data.Repository import repo_functions as rf
 def add_product_order(product_order_id, quantity_limit, order_amount, order_incoming_date):
     rf.add_model(ProductOrder, product_order_id=product_order_id, quantity_limit=quantity_limit,
                  order_amount=order_amount, order_incoming_date=order_incoming_date)
+
+
+def get_product_order_by_id(product_order_id):
+    return rf.get_model_by_id(ProductOrder, 'product_order_id', product_order_id)
