c5eb1a355709a0c9a65fb3fa5d370e6061fde9cf
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-05 12:04:07 +0100

    Changed product_orders to be 1-1 relationship with products

--- app/Data/models/product_order.py
+++ app/Data/models/product_order.py
@@ -6,14 +6,13 @@ from sqlalchemy.orm import relationship
 class ProductOrder(Base):
     __tablename__ = 'product_orders'
 
-    product_order_id = sa.Column(sa.Integer, primary_key=True)
+    product_order_id = sa.Column(sa.Integer, sa.ForeignKey('products.product_id'), primary_key=True)
     quantity_limit = sa.Column(sa.Integer, nullable=False)
     order_amount = sa.Column(sa.Integer, nullable=False)
     order_incoming_date = sa.Column(sa.Date, nullable=False)
-    product_order_product_id = sa.Column(sa.Integer, sa.ForeignKey('products_product_id'),  nullable=False)
 
     # product = relationship("Product", back_populates="product_order")
 
     def __repr__(self):
-        return f'product_order: {self.product_order_id}, {self.quantity_limit}, {self.order_amount},' \
-               f'{self.order_incoming_date}, {self.product_order_product_id}'
+        return f'ProductOrder: {self.product_order_id}, {self.quantity_limit}, {self.order_amount},' \
+               f'{self.order_incoming_date}'
