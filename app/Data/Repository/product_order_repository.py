eceecd67c4c209c6095e06d28a98aa8a742c311d
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-05 12:04:27 +0100

    Added product_order_repository

---
+++ app/Data/Repository/product_order_repository.py
@@ -0,0 +1,7 @@
+from Data.models.product_order import ProductOrder
+from Data.Repository import repo_functions as rf
+
+
+def add_product_order(product_order_id, quantity_limit, order_amount, order_incoming_date):
+    rf.add_model(ProductOrder, product_order_id=product_order_id, quantity_limit=quantity_limit,
+                 order_amount=order_amount, order_incoming_date=order_incoming_date)
