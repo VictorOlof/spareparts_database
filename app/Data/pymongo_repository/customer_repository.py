3258443e380087e6e63529b94fdda8012c434e81
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-07 20:15:51 +0100

    Added usage of repo_functions

--- app/Data/pymongo_repository/customer_repository.py
+++ app/Data/pymongo_repository/customer_repository.py
@@ -3,10 +3,9 @@ from Data.pymongo_repository import repo_functions as rf
 
 
 def add_customer(first_name, last_name, address_name, city, postal_code, country, company_name, org_number):
-    customer = mm.Customer({'first_name': first_name, 'last_name': last_name, 'address': address_name,
-                            'city': city, 'postal_code': postal_code, 'country': country,
-                            'company_name': company_name, 'org_number': org_number})
-    customer.save()
+    rf.add_model(mm.Customer, first_name=first_name, last_name=last_name, address=address_name,
+                 city=city, postal_code=postal_code, country=country, company_name=company_name,
+                 org_number=org_number)
 
 
 def get_all_customers():
@@ -17,16 +16,12 @@ def get_customer_by_id(customer_id):
     return rf.get_model_by_id(mm.Customer, customer_id)
 
 
-def get_customers_by_column_value(column_value, value):
-    pass
-
-
 def update_customer_by_column(customer_obj, column_value, value):
     rf.update_object_by_column(customer_obj, column_value, value)
 
 
 def remove_customer(customer_obj):
-    pass
+    rf.remove_object(customer_obj)
 
 
 def get_customer_by_car(car_obj):
