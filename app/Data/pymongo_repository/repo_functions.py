1648cbc4c585fed7d4025ae62dca02f8c91eb7cb
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-07 20:17:41 +0100

    Added get_object_columns, get_model_by_column_value, add_model

--- app/Data/pymongo_repository/repo_functions.py
+++ app/Data/pymongo_repository/repo_functions.py
@@ -1,6 +1,11 @@
 from bson import ObjectId
 
 
+def add_model(model_obj, **kwargs):
+    obj_temp = model_obj(kwargs)
+    obj_temp.save()
+
+
 def get_all_models(model_obj):
     try:
         return model_obj.all()
@@ -15,13 +20,30 @@ def get_model_by_id(model_obj, value):
         return None
 
 
+def get_model_by_column_value(model_obj, column_value, value):
+    try:
+        return model_obj.find(**{column_value: value}).first_or_none()
+    except:
+        return None
+
+
 def get_models_by_column_value(model_obj, column_value, value):
-    pass
+    try:
+        return model_obj.find(**{column_value: value})
+    except:
+        return None
 
 
 def update_object_by_column(model_obj, column_value, value):
-    return model_obj.update_field(column_value, value)
+    try:
+        return model_obj.update_field(column_value, value)
+    except:
+        return None
 
 
 def remove_object(model_obj):
+    model_obj.remove_doc()
+
+
+def get_object_columns(model_obj):
     pass
