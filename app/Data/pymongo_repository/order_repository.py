55af429dc9deb7905f05650c6128df142a1d1f23
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-08 02:27:10 +0100

    Added use of repo_functions

--- app/Data/pymongo_repository/order_repository.py
+++ app/Data/pymongo_repository/order_repository.py
@@ -1,16 +1,24 @@
 import Data.pymongo_models as mm
 from Data.pymongo_repository import repo_functions as rf
+from bson import ObjectId
 
 
 def create_order(order_date, required_date, shipped_date, status, comment, employee_id, customer_id):
-    order = mm.Order({'order_date': order_date, 'required_date': required_date, 'shipped_date': shipped_date,
-                      'status': status, 'comment': comment, 'employee_id': employee_id, 'customer_id': customer_id})
-    order.save()
+    return rf.add_model(mm.Order, order_date=order_date, required_date=required_date, shipped_date=shipped_date,
+                        status=status, comment=comment, employee_id=employee_id, customer_id=customer_id)
 
 
 def add_item_to_order(order_id, product_id, quantity_ordered, sell_price_each):
-    pass
+    rf.insert_items_to_embedded_list(mm.Order, order_id, 'order_details', {'product_id': product_id,
+                                                                           'quantity_ordered': quantity_ordered,
+                                                                           'sell_price_each': sell_price_each})
 
 
 def get_all_orders():
     return rf.get_all_models(mm.Order)
+
+
+def add_customer_to_order(order_id, customer_id):
+    customer = rf.get_model_by_id(mm.Customer, customer_id)
+    rf.insert_items_to_embedded_field(mm.Order, order_id, 'customer', {'first_name': customer.first_name,
+                                                                       'last_name': customer.last_name})
