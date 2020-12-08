0d8455caaff36f2eb6cc3a1796f10e7823caab19
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-08 10:41:05 +0100

    Moved add_customer_to_order controller->repository

--- app/Controllers/orders_controller.py
+++ app/Controllers/orders_controller.py
@@ -2,8 +2,7 @@ import Data.pymongo_repository.order_repository as p_o
 
 
 def create_order(order_date, required_date, shipped_date, status, comment, employee_id, customer_id):
-    obj_temp = p_o.create_order(order_date, required_date, shipped_date, status, comment, employee_id, customer_id)
-    p_o.add_customer_to_order(obj_temp.order_id, customer_id)
+    p_o.create_order(order_date, required_date, shipped_date, status, comment, employee_id, customer_id)
 
 
 def add_item_to_order(order_id, product_id, quantity_ordered, sell_price_each):
--- app/Data/pymongo_repository/order_repository.py
+++ app/Data/pymongo_repository/order_repository.py
@@ -4,8 +4,9 @@ from bson import ObjectId
 
 
 def create_order(order_date, required_date, shipped_date, status, comment, employee_id, customer_id):
-    return rf.add_model(mm.Order, order_date=order_date, required_date=required_date, shipped_date=shipped_date,
-                        status=status, comment=comment, employee_id=employee_id, customer_id=customer_id)
+    obj_temp = rf.add_model(mm.Order, order_date=order_date, required_date=required_date, shipped_date=shipped_date,
+                            status=status, comment=comment, employee_id=employee_id, customer_id=customer_id)
+    add_customer_to_order(obj_temp.order_id, customer_id)
 
 
 def add_item_to_order(order_id, product_id, quantity_ordered, sell_price_each):
