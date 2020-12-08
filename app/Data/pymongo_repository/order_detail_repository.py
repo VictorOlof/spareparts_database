df1540285da24d8b4aba74a120f781ef30566eb8
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-08 13:07:25 +0100

    Fixed add_order_detail

--- app/Data/pymongo_repository/order_detail_repository.py
+++ app/Data/pymongo_repository/order_detail_repository.py
@@ -3,11 +3,11 @@ from Data.pymongo_repository import repo_functions as rf
 
 
 def add_order_detail(order_id, product_id, quantity_ordered, sell_price_each):
-    pass
-    """order_detail = OrderDetail(order_id=order_id, product_id=product_id, quantity_ordered=quantity_ordered,
-                               sell_price_each=sell_price_each)
-    session.add(order_detail)
-    session.commit()"""
+    product = rf.get_model_by_id(mm.Product, product_id)
+    rf.insert_items_to_embedded_list(mm.Order, order_id, 'order_details', {'product_id': product_id,
+                                                                           'product_name': product.product_name,
+                                                                           'quantity_ordered': quantity_ordered,
+                                                                           'sell_price_each': sell_price_each})
 
 
 def get_all_order_details(order_id):
