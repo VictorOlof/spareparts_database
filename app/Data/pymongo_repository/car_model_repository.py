2cbfd870c03d0d28090742febe5c123eed761a18
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-09 20:12:24 +0100

    Fixed add_car_model

--- app/Data/pymongo_repository/car_model_repository.py
+++ app/Data/pymongo_repository/car_model_repository.py
@@ -2,13 +2,12 @@ import Data.pymongo_models as mm
 from Data.pymongo_repository import repo_functions as rf
 
 
-def add_car_model(model_obj, car_model_name, car_model_year):
-    rf.update_object_by_column(model_obj=model_obj,
-                               column_value='car_model_name',
-                               value=car_model_name)
-    rf.update_object_by_column(model_obj=model_obj,
-                               column_value='car_model_year',
-                               value=car_model_year)
+def add_car_model(car_model_name, car_model_year, car_model_car_brand_id):
+    obj_temp = rf.get_model_by_id(mm.CarModel, car_model_car_brand_id)
+    rf.add_model(mm.CarModel,
+                 car_brand_name=obj_temp.car_brand_name,
+                 car_model_name=car_model_name,
+                 car_model_year=car_model_year)
 
 
 def get_all_car_models():
