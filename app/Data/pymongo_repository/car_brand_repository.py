023463407d3204655fd84b20acf9a370ac2b9f17
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-09 15:52:18 +0100

    Init car_brand_repo

---
+++ app/Data/pymongo_repository/car_brand_repository.py
@@ -0,0 +1,14 @@
+import Data.pymongo_models as mm
+from Data.pymongo_repository import repo_functions as rf
+
+
+def add_car_brand(car_brand_name):
+    return rf.add_model(mm.CarBrand, car_brand_name=car_brand_name)
+
+
+def get_all_car_brands():
+    pass  # return rf.get_all_models(CarBrand)
+
+
+def get_brand_by_model(car_model_obj):
+    pass  # return car_model_obj.car_brand
