b0b308e31ceb5c4b9d1c385825f42c8d15806f8c
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-09 20:12:57 +0100

    Fixed controller for pymongo

--- app/Controllers/carbrands_controller.py
+++ app/Controllers/carbrands_controller.py
@@ -1,14 +1,16 @@
-import Data.Repository.car_brand_repository as cbr
+import Data.pymongo_repository.car_brand_repository as p_cb
 
 
 def add_car_brand(car_brand_name):
-    cbr.add_car_brand(car_brand_name)
+    p_cb.add_car_brand(car_brand_name)
 
 
 def get_all_car_brands():
-    car_brands = cbr.get_all_car_brands()
+    car_brands = p_cb.get_all_car_brands()
     return {str(i+1): car_brand for i, car_brand in enumerate(car_brands)}
+    # return {str(i+1): car_brand for i, car_brand in enumerate(car_brands)}
 
 
 def get_brand_by_model(car_model_obj):
-    return cbr.get_brand_by_model(car_model_obj)
+    pass
+    # return cbr.get_brand_by_model(car_model_obj)
