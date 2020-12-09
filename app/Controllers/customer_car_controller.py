6317615d031514e8ce0a7ab526aa23dad70109c0
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-09 22:46:20 +0100

    Fixed add_customer_car

--- app/Controllers/customer_car_controller.py
+++ app/Controllers/customer_car_controller.py
@@ -1,21 +1,21 @@
-import Data.Repository.customer_car_repository as ccr
+import Data.pymongo_repository.customer_car_repository as p_cc
 
 
 def add_customer_car(customer_id, car_model_id, reg_plate, color):
-    ccr.add_customer_car(customer_id, car_model_id, reg_plate, color)
+    p_cc.add_customer_car(customer_id, car_model_id, reg_plate, color)
 
 
 def get_all_customer_car():
-    return ccr.get_all_customer_car()
+    pass  # return ccr.get_all_customer_car()
 
 
 def get_customer_car_by_id(customer_id):
-    return ccr.get_customer_car_by_id(customer_id)
+    pass  # return ccr.get_customer_car_by_id(customer_id)
 
 
 def get_car_by_reg_plate(reg_plate):
-    return ccr.get_car_by_reg_plate(reg_plate)
+    pass  # return ccr.get_car_by_reg_plate(reg_plate)
 
 
 def get_customer_by_customer_car(customer_car_obj):
-    return ccr.get_customer_by_customer_car(customer_car_obj)
+    pass  # return ccr.get_customer_by_customer_car(customer_car_obj)
