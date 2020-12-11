54dd8cb5749ffcddf085464d53f323e224afed83
Victor Olofsson <victorolofsson@hotmail.com>
2020-12-11 13:34:02 +0100

    Added get_object_info

--- app/UI/menu_functions.py
+++ app/UI/menu_functions.py
@@ -19,13 +19,13 @@ def print_table(table: list):
     """Takes a list of dictionaries and print out values of dict in a table format"""
     # print table head row
     for key in table[0].keys():
-        print('{:20}'.format(key), end="")
+        print('{:25}'.format(str(key)), end="")
     print()
     # print rows
     for dic in table:
         for key in dic:
-            value = dic[key]
-            print('{:20}'.format(value), end="")
+            value = str(dic[key])
+            print('{:25}'.format(value), end="")
         print()
 
 
@@ -33,3 +33,13 @@ def print_all_key_value_in_dict(dictionary: dict):
     """Prints key and value of dict in format (KEY. VALUE)"""
     for key, value in dictionary.items():
         print(f'{key}. {value}')
+
+
+def get_object_info(columns, column_skip=()):
+    obj_info = []
+    for column in columns:
+        if column not in column_skip:
+            column_print = column.replace("_", " ")
+            value = input(f"Enter {column_print}: ")
+            obj_info.append(value)
+    return obj_info
